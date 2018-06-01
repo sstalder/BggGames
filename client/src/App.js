a546a1ce81c27bcd4a130a0a5327a4e6b0ed747b
Steve <sstalder@gmail.com>
2018-06-01 17:13:25 -0400

    Add debouncing / refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import { Form, FormGroup, Label, Input, Alert, ListGroup, ListGroupItem, Badge } from 'reactstrap';
+
 import './App.css';
-import { Form, FormGroup, Label, Input, Alert, ListGroup, ListGroupItem } from 'reactstrap';
 
 class App extends Component {
   constructor(props) {
@@ -18,13 +20,15 @@ class App extends Component {
     this.setState({ games: data });
   }
 
-  onSearch = async (e) => {
-    const term = e.target.value;
+  onSearch = async () => {
+    const term = this.search.value;
     const request = await fetch(`http://localhost:5000/api/games/${term}`);
     const data = await request.json();
 
     this.setState({ games: data });
   }
+
+  onSearchDebounced = _.debounce(this.onSearch, 250)
 
   render() {
     return (
@@ -37,16 +41,24 @@ class App extends Component {
           <Form>
             <FormGroup>
               <Label for="search" hidden>Search</Label>
-              <Input type="text" placeholder="Search for a game..." onChange={this.onSearch} />
+              <Input type="text" placeholder="Search for a game..." 
+                innerRef={ref => { this.search = ref; }}
+                onChange={this.onSearchDebounced} />
             </FormGroup>
           </Form>
 
           {this.state.games.length > 0 ? (
-            <ListGroup>
-              {this.state.games.map((game) => (
-                <ListGroupItem key={game.id}>{game.title}</ListGroupItem>
-              ))}
-            </ListGroup>
+            <div>
+              <p>
+                <Badge color="dark">Found {this.state.games.length} results</Badge>
+              </p>
+              
+              <ListGroup>
+                {this.state.games.map((game) => (
+                  <ListGroupItem key={game.id}>{game.title}</ListGroupItem>
+                ))}
+              </ListGroup>
+            </div>
           ) : (
             <Alert color="secondary">No games found for your search...</Alert>
           )}
