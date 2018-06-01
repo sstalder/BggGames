1fff2d321772579c73ade4705ca7133ac49cc9ac
Steve Stalder <sstalder@gmail.com>
2018-06-01 12:11:56 -0400

    Start on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Form, FormGroup, Label, Input, Alert, ListGroup, ListGroupItem } from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      games: []
+    };
+  }
+
+  componentWillMount = async () => {
+    const request = await fetch('http://localhost:5000/api/games');
+    const data = await request.json();
+
+    this.setState({ games: data });
+  }
+
+  onSearch = async (e) => {
+    const term = e.target.value;
+    const request = await fetch(`http://localhost:5000/api/games/${term}`);
+    const data = await request.json();
+
+    this.setState({ games: data });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Search some games & stuff</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="container">
+          <Form>
+            <FormGroup>
+              <Label for="search" hidden>Search</Label>
+              <Input type="text" placeholder="Search for a game..." onChange={this.onSearch} />
+            </FormGroup>
+          </Form>
+
+          {this.state.games.length > 0 ? (
+            <ListGroup>
+              {this.state.games.map((game) => (
+                <ListGroupItem key={game.id}>{game.title}</ListGroupItem>
+              ))}
+            </ListGroup>
+          ) : (
+            <Alert color="secondary">No games found for your search...</Alert>
+          )}
+        </div>
       </div>
     );
   }
